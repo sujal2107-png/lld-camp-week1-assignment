b65020f58243d643a6f875146bbd546226f4cc97
sujal2107-png <sujal1104@gmail.com>
2025-11-14 12:59:43 +0530

    10/10

--- Cpp/02-media-lsp-isp.cpp
+++ Cpp/02-media-lsp-isp.cpp
@@ -7,32 +7,47 @@
 
 using namespace std;
 
-class Player {
-public:
-    virtual ~Player() = default;
+class IPlayable{
+public:    
+    virtual ~IPlayable() = default;
     virtual void play(const string& source) = 0;
+};
+class IPausable{
+public:     
+    virtual ~IPausable() = default;
     virtual void pause() = 0;
-    virtual void record(const string& destination) = 0;  // many can't
-    virtual void streamLive(const string& url) = 0;       // many can't
-    virtual void download(const string& sourceUrl) = 0;   // many can't
+};
+class IRecordable{
+public:     
+    virtual ~IRecordable() = default;
+    virtual void record(const string& destination) = 0; 
+};
+class ILiveStreamable{
+public:     
+    virtual ~ILiveStreamable() = default;
+    virtual void streamLive(const string& url) = 0; 
+};
+class IDownloadable{
+public:     
+    virtual ~IDownloadable() = default;
+    virtual void download(const string& url) = 0; 
 };
 
-class AudioPlayer : public Player {
+
+class AudioPlayer : public IPlayable,IPausable,IDownloadable {
     bool playing{false};
 public:
     void play(const string& source) override { (void)source; playing = true; }
     void pause() override { playing = false; }
-    void record(const string&) override { throw logic_error("AudioPlayer cannot record"); }       // LSP break
-    void streamLive(const string&) override { throw logic_error("AudioPlayer cannot streamLive"); } // LSP break
-    void download(const string& url) override { (void)url; /* pretend */ }
+    void download(const string& url)override{ (void)url; /* pretend */ }
     bool isPlaying() const { return playing; }
 };
 
-class CameraStreamPlayer : public Player {
+class CameraStreamPlayer : public IPausable,IRecordable,ILiveStreamable {
     bool liveStarted{false};
     bool playing{false};
 public:
-    void play(const string& src) override {
+    void play(const string& src){
         (void)src;
         // Surprise: needs streamLive first for “real” play
         if (!liveStarted) {
@@ -43,7 +58,6 @@ public:
     void pause() override { playing = false; }
     void record(const string& dest) override { (void)dest; /* pretend */ }
     void streamLive(const string& url) override { (void)url; liveStarted = true; }
-    void download(const string&) override { throw logic_error("CameraStreamPlayer cannot download"); } // LSP break
     bool isPlaying() const { return playing; }
     bool isLive() const { return liveStarted; }
 };
@@ -54,12 +68,8 @@ int main() {
     cout << "Audio playing: " << boolalpha << ap.isPlaying() << "\n";
     ap.pause();
 
-    CameraStreamPlayer cam;
-    cam.play("rtsp://camera");       // warning surprise
+    CameraStreamPlayer cam;      // warning surprise
     cam.streamLive("rtsp://camera"); // required order
     cam.play("rtsp://camera");
-    try { cam.download("http://file"); } catch (const exception& e) {
-        cout << "[EXC] " << e.what() << "\n";
-    }
     return 0;
 }
